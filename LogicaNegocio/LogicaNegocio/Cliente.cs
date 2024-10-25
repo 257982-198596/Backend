4aca6df6b87af6fec5e938a0a4fdf1b92beb8289
zionweb <92651245+zionweb@users.noreply.github.com>
2024-10-25 20:30:24 -0300

    Primer commit de inicio de proyecto (Clase Cliente-Estado)

---
+++ LogicaNegocio/LogicaNegocio/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaNegocio
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public String NombreEmpresa { get; set; }
+
+        public String RUToCedula { get; set; }
+
+        public String PersonaContacto { get; set; }
+
+        public String TelefonoMovil { get; set; }
+
+        public String Direccion { get; set; }
+
+        public EstadoCliente Estado { get; set; }
+    }
+}
